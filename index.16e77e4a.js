cce56c8bcd40e1dd5d31c9793d2fd408cc05a052
Nora Audonnet <noraaudonnet@gmail.com>
2024-06-13 19:59:50 +0300

    make build

---
+++ index.16e77e4a.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link");r&&(e.src=r.href)});
+//# sourceMappingURL=index.16e77e4a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const mainImage = document.querySelector("#largeImg");
+const thumbs = document.querySelector("#thumbs");
+// eslint-disable-next-line no-shadow
+thumbs.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const li = event.target.closest(".list-item__link");
+    if (!li) return;
+    mainImage.src = li.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
